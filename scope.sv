f29f7bf3da2b645e942ab188659afd9c720da6ae
ishraqk <50222375+ishraqk@users.noreply.github.com>
2019-05-09 18:17:44 -0400

    fixed instantiation of adc module -- added s_clk

--- scope.sv
+++ scope.sv
@@ -26,18 +26,29 @@ module scope(	input logic        	clk,
 				output logic [6:0] 	HEX0, HEX1, HEX2, HEX3, HEX4, HEX5
 			);
 
+
+	logic	[11:0]	ADC_Sample; //12 bit binary value coming from ADC
+
+
 	//initialize the values that will eventually be communicated through software
 	// trigger, horizontal_sweep, vertical_sweep
+	logic trigger;
+	logic rising; //if 1, rising edge, else falling
+	logic full_flag;
+	
+	assign rising = 1;
+	assign full_flag = 0;
 
 	//instantiate the adc
-	adc adc0 (.CLOCK_50(clk), .ADC_CS_N(ADC_CS_N), .ADC_DIN(ADC_DIN), 
+	adc adc0 (.CLOCK_50(clk), .ADC_CS_N(ADC_CS_N), .ADC_DIN(ADC_DIN), .ADC_SCLK(ADC_SCLK),
 			.HEX0(HEX0), .HEX1(HEX1), .HEX2(HEX2), .HEX3(HEX3), .HEX4(HEX4), .HEX5(HEX5), .ADC_DOUT(ADC_DOUT));
-
+	
+	assign ADC_Sample=ADC
+	
+	//instantiate vga
 	vga_ball v0 (.clk(clk), .reset(reset), .writedata(writedata), 
-			.write(write), .chipselect(chipselect), .VGA_R(VGA_R), .VGA_G(VGA_G), .VGA_B(VGA_B), .VGA_CLK(VGA_CLK), .VGA_HS(VGA_HS), .VGA_VS(VGA_VS), .VGA_BLANK_n(VGA_BLANK_n) , .VGA_SYNC_n(VGA_SYNC_n) );
+			.write(write), .chipselect(chipselect), .VGA_R(VGA_R), .VGA_G(VGA_G), .VGA_B(VGA_B), .VGA_CLK(VGA_CLK), .VGA_HS(VGA_HS), .VGA_VS(VGA_VS), .VGA_BLANK_n(VGA_BLANK_n) , 
+			.VGA_SYNC_n(VGA_SYNC_n) ); //.sample(ADC_Sample), .full(full_flag)
 
-
-	//instantiate the vga
-	//vga_ball v0 ();
 
 endmodule
