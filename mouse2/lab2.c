76035dbe5015dce363ea906e5dc20423070132f1
zhongtai <zr2208@columbia.edu>
2019-05-10 16:19:30 -0400

    mouse function

---
+++ mouse2/lab2.c
@@ -0,0 +1,22 @@
+#include "usbmouse.h"
+#include "read_mouse.h"
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main(void)
+{
+	/* code */
+	struct mouse_info mouse0;
+	init_mouse();
+
+	for(;;){
+		read_mouse(&mouse0);
+
+		printf("position of x, y are: %d %d; left click is %d\n",mouse0.x,mouse0.y,mouse0.button);
+	}
+
+	return 0;
+}
